99e7a8afafc3a9ac586c3e546035f1560c2cfc84
Abu Shaid <mdabushaid847@gmail.com>
2026-08-28 11:35:08 +0600

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+let todoForm = document.getElementById("todoForm")
+let searchInput = document.getElementById("searchInput");
+let allBtn = document.getElementById("allBtn");
+let completedBtn = document.getElementById("completedBtn");
+let pendingBtn = document.getElementById("pendingBtn");
+let totalCount = document.getElementById("totalCount");
+let completedCount = document.getElementById("completedCount");
+let pendingCount = document.getElementById("pendingCount");
+let todoList = document.getElementById("todoList");
+
