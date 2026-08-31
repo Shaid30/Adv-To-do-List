3a16f997113c7146b33601abc33edbdd2bfbf19f
Abu Shaid <mdabushaid847@gmail.com>
2026-08-31 12:31:08 +0600

    add task complete

--- script.js
+++ script.js
@@ -7,17 +7,25 @@ let totalCount = document.getElementById("totalCount");
 let completedCount = document.getElementById("completedCount");
 let pendingCount = document.getElementById("pendingCount");
 let todoList = document.getElementById("todoList");
+let addTask = document.getElementById("addTask");
 
 let tasks =[];
 
 todoForm.addEventListener("submit", function(event){
     event.preventDefault()
-    addTask.value
     let task = addTask.value;
     tasks.push(task)
+    displayTask();
 
 });
 
 function displayTask(){
-    
+    todoList.innerHTML = "";
+    tasks.forEach(function(task){
+        let li = document.createElement("li")
+        li.innerText = task
+        todoList.appendChild(li);
+        
+    })
+        
 }
